57fe3acb630208b6341b39965103d494a084821f
loststarrs <chandruharsha8@gmail.com>
2025-12-30 11:54:18 +0530

    Fix typo in output message

--- src/main/java/com/example/Hello.java
+++ src/main/java/com/example/Hello.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins nd Maven!");
+        System.out.println("Hello enkins nd Maven!");
     }
 }
